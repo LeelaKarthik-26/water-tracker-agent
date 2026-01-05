a646d84bedd07bb1df2e29b411638795a64abfe8
LeelaKarthik-26 <leelakarthik101@gmail.com>
2026-01-04 21:18:34 -0500

    database Error updates

--- src/database.py
+++ src/database.py
@@ -7,11 +7,11 @@ def create_tables():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS wate_intake(
+        CREATE TABLE IF NOT EXISTS water_intake(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id TEXT,
             intake_ml INTERGER,
-            data TEXT
+            date TEXT
         )               
     """)
     conn.commit()
@@ -29,7 +29,7 @@ def log_intake(user_id, intake_ml):
 def get_intake_history(user_id):
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
-    cursor.execute("SELECT intake_ml, data from water_intake WHERE user_id=?", user_id)
+    cursor.execute("SELECT intake_ml, date FROM water_intake WHERE user_id=?", (user_id, ),)
     records = cursor.fetchall()
     conn.close()
     return records
