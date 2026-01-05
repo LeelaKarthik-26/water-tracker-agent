6bf31549113afb2dd318d625a13909728bc9d15a
LeelaKarthik-26 <leelakarthik101@gmail.com>
2026-01-04 20:55:12 -0500

    web api  using fastapi

--- src/agent.py
+++ src/agent.py
@@ -1,12 +1,12 @@
 import os
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import init_chat_model
 from langchain.messages import HumanMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(api_key=OPENAI_API_KEY, model='gpt-4', temperature=0.5)
+llm = init_chat_model(api_key=OPENAI_API_KEY, model='gpt-4', model_provider='openai', temperature=0.5)
 
 class WaterIntakeAgent:
     
